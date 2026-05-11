eaab3eee9fd8d062405973d8f30d38bfe63bea02
InakiMedina <inaki.medina@gmail.com>
2026-05-10 20:00:40 -0600

    now output.tf was wrong, fixed, includes ec2 isntance id

--- terraform/output.tf
+++ terraform/output.tf
@@ -5,3 +5,8 @@ output "sales_instance_id" {
 output "sales_public_ip" {
   value = aws_instance.sales_service.public_ip
 }
+
+output "ec2_instance_id" {
+  description = "The ID of the EC2 instance for the Sales Service"
+  value       = aws_instance.sales_service.id
+}
