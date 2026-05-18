805cc56995adefe6a9afb1f9a8f0311023d81977
InakiMedina <inaki.medina@gmail.com>
2026-05-17 19:52:46 -0600

    sends eips to core

--- terraform/output.tf
+++ terraform/output.tf
@@ -7,3 +7,8 @@ output "sales_public_ip" {
   description = "Stable public IPv4 (Elastic IP) for Sales"
   value       = aws_eip.sales.public_ip
 }
+
+output "sales_backend_url" {
+  description = "Base URL written to Core SALES_BACKEND_URL"
+  value       = "http://${aws_eip.sales.public_ip}:80"
+}
